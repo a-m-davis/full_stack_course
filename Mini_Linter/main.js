bb85f2f2f4f694074922fa94e2e53d1fb7f313f9
elliottdavis <elliott@tirefire.dev>
2022-01-07 12:26:33 -0600

    Rubbed some regex on it

--- Mini_Linter/main.js
+++ Mini_Linter/main.js
@@ -1,55 +1,26 @@
 let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
-let overusedWords = ['really', 'very', 'basically'];
-
 let unnecessaryWords = ['extremely', 'literally', 'actually' ];
 
+const overusedWordsObject = { really: 0, very: 0, basically: 0 };
 
-const storyWords = story.split(' ');
-//console.log(storyWords.length);
+const storyWords = story.split(/\W+/);
 
 const betterWords = storyWords.filter(word => {
   return !unnecessaryWords.includes(word);
 });
 
-// console.log(betterWords);
-
-const overusedWordsObject = {
-  really: 0,
-  very: 0,
-  basically: 0,
-};
-
-for (word of storyWords){
+for (const word of storyWords){
   if (word in overusedWordsObject){
     overusedWordsObject[word]++
   }
 };
 
-// console.log(reallyCount, veryCount, basicallyCount);
-
-let periodCount = 0;
-let exclamationCount = 0;
-let questionCount = 0;
-
-for (word of storyWords){
-  if (word.includes('.')){
-    periodCount += 1;
-  } else if (word.includes('!')){
-    exclamationCount += 1;
-  } else if (word.includes('?')){
-    questionCount +=1;
-  }
-};
-
-// console.log(periodCount, exclamationCount, questionCount);
-let totalSentences = periodCount + exclamationCount + questionCount
-
 console.log('The total number of words is: ', storyWords.length);
-console.log('The total number of sentences is: ', totalSentences);
+console.log('The total number of sentences is: ', (story.match(/([\.\?!])/g) || []).length);
 
 for (let key in overusedWordsObject){
-  console.log(`The number of time '${key}' was used: ${overusedWordsObject[key]}`)
+  console.log(`The number of times '${key}' was used: ${overusedWordsObject[key]}`)
 };
 
 console.log(betterWords.join(' '));
